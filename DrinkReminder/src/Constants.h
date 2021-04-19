2ff2bc9b9d62ad444d8e8bd231a73a1ac9c9b925
Pascal.Strobel <53040413+paschistrobel@users.noreply.github.com>
2021-04-19 21:37:10 +0200

    Programmlogik implementiert
    Noch nicht final und auch noch nicht auf Bugs getestet!

--- DrinkReminder/src/Constants.h
+++ DrinkReminder/src/Constants.h
@@ -1,12 +1,11 @@
 //Input PINs
-#define FLOW_SENSOR_PIN A1 // analog input
-#define PRESSURE_SENSOR_PIN A3 // analog input
-
+#define FLOW_SENSOR_PIN 7 // digital input
+#define PRESSURE_SENSOR_PIN A2 // analog input
 //Output PINs
-#define BOTTLE_STRIP_PIN 2 // digital output
-#define GLASS_STRIP_PIN 4 // digital output
-#define SPEAKER_PIN 6 // digital output
-#define WATER_PUMP_PIN 8 // digital output
+#define WATER_PUMP_PIN 2 // digital output
+#define GLASS_STRIP_PIN 3 // digital output
+#define SPEAKER_PIN 4 // digital output
+#define BOTTLE_STRIP_PIN 5 // digital output
 
 //LED strips
 #define BOTTLE_STRIP_COUNT 9
@@ -17,11 +16,22 @@
 #define TONE_FREQUENCY 300
 #define TONE_DURATION 1000 // duration in ms
 
+//States
+#define STANDARD 0
+#define RECHARGING 1
+#define BOTTLE_EMPTY 2
+#define NO_GLASS 3
+
+//Glass pressure values
+#define THRESHOLD_NO_GLASS 3 // until this threshold the glass status is defined as no glass
+#define THRESHOLD_EMPTY_GLASS 10 // until this threshold the glass status is defined as empty; if higher than this value, then theres still water in the glass
 
 
-//Other
-#define HOURS_PER_DAY 24
-#define SLEEP_HOURS_PER_DAY 8
-#define DAILY_WATER_AMOUNT 3000 // in milliliters
-#define REMIND_TIMESTEPS 30 // in minutes
-#define SNOOZE_TIMESTEPS 2 // in minutes
+//Time Intervals and water amount
+//#define REMIND_INTERVAL 1800000  // 30 mins * 60 * 1000 (= milliseconds)
+//#define SNOOZE_INTERVAL 120000 // 2 mins * 60 * 1000 // not exactly 2 mins to avoid overlaps with REMIND_INTERVAL
+#define WATER_AMOUNT 80 // Water amound (in ml) that has to be consumed within the REMIND_INTERVAL
+
+//
+#define REMIND_INTERVAL 20000
+#define SNOOZE_INTERVAL 5010
--- DrinkReminder/src/main.cpp
+++ DrinkReminder/src/main.cpp
@@ -1,5 +1,6 @@
-/*Benötigte Libraries
-- Adafruit NeoPixel
+/*
+Required Libraries
+	- Adafruit NeoPixel
 */
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
@@ -7,6 +8,15 @@
 
 Adafruit_NeoPixel bottleStrip(BOTTLE_STRIP_COUNT, BOTTLE_STRIP_PIN, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel glassStrip(GLASS_STRIP_COUNT, GLASS_STRIP_PIN, NEO_GRB + NEO_KHZ800);
+
+int deviceState; // state of the device
+int waterAmount_required; // the amount of water (in ml) the users should have drunk so far
+int waterAmount_drunk; // the amount of water (in ml) that has been drunk so far
+volatile double waterFlow; // amount of water (in ml) pumped in the current filling process
+long lastMillis; // the past milliseconds when you were reminded to drink the last time
+long lastSnoozeMillis; // the past milliseconds when the snooze remind was active the last time
+bool fillingInterrupted; // was the last filling process interrupted
+bool wasFilledUp; // was the glass filled up at least once
 
 void initPINS(){
 	pinMode(FLOW_SENSOR_PIN, INPUT);
@@ -22,11 +32,22 @@ void initObjects(){
 	glassStrip.show(); // Turn OFF all pixels ASAP
 	glassStrip.setBrightness(LED_BRIGHTNESS); // Set BRIGHTNESS to about 1/5 (max = 255)
 }
+void initValues(){
+	deviceState = STANDARD;
+	lastMillis = millis();
+	waterAmount_required = 0;
+	waterAmount_drunk = 0;
+	waterFlow = 0;
+	fillingInterrupted = false;
+	wasFilledUp = false;
+}
 
+// method for sound
 void beep(){
 		tone(SPEAKER_PIN, TONE_FREQUENCY, TONE_DURATION);
 }
-void LED_loading_animation(Adafruit_NeoPixel &strip, uint32_t color, int wait){
+// pixel runs from left to right of the strip
+void runningPixel(Adafruit_NeoPixel &strip, uint32_t color, int wait){
 	for(int i = 0; i < strip.numPixels(); i++){
 		strip.clear();
 		strip.setPixelColor(i, color);
@@ -40,17 +61,127 @@ void LED_loading_animation(Adafruit_NeoPixel &strip, uint32_t color, int wait){
 		delay(wait);
 	}
 }
+// blink LED for 1 second
+void blink(Adafruit_NeoPixel &strip, uint32_t color){
+	strip.clear();
+	for(int i = 0;  i < strip.numPixels(); i++){
+		strip.setPixelColor(i, color);
+	}
+	strip.show();
+	delay(1000);
+	strip.clear();
+	strip.show();
+}
 
-void setup() {
-	Serial.begin(9600);
-	initPINS();
-	initObjects();
+void remindToDrink(){
+	Serial.println("Remind to drink");
+	beep();
+	blink(glassStrip, glassStrip.Color(255, 0, 0));
+}
+
+// returns state of the glass: -1 if glass not on plate, 0 if glass on plate and empty, 1 if glass filled with water
+int getGlassState() {
+	int val = analogRead(PRESSURE_SENSOR_PIN);
+	if(val <= THRESHOLD_NO_GLASS){
+		return -1;
+	}
+	else if(val > THRESHOLD_NO_GLASS && val <= THRESHOLD_EMPTY_GLASS){
+		return 0;
+	}
+	else{
+		return 1;
+	}
+}
+
+bool bottleEmpty(){
+	// read value from flow sensor
+	if (true) {
+		return true;
+	}
+	return false;
+}
+
+void pulse() {  // measure the quantity of square waves
+	waterFlow += 1.0 / 5880.0; // 5880 is the number of Pulses required for one liter
 }
 
 
-// Thread option: https://create.arduino.cc/projecthub/reanimationxp/how-to-multithread-an-arduino-protothreading-tutorial-dd2c37
-void loop() {
-	Serial.println(1);
-	delay(200);
 
+
+void setup() {
+	Serial.begin(9600);
+	delay(2000);
+	Serial.println("Start");
+	initPINS();
+	initObjects();
+	initValues();
+}
+
+void loop() {
+	// remind timers work independent of device status
+	if (millis() - lastMillis >= REMIND_INTERVAL) { // time to remind the user to drink
+		Serial.println("Alarm Alarm Alarm Alarm!!!!");
+		waterAmount_required += WATER_AMOUNT;
+		remindToDrink();
+		lastMillis = millis();
+		lastSnoozeMillis = millis();
+	}
+	else if (waterAmount_drunk < waterAmount_required && millis()  - lastSnoozeMillis >= SNOOZE_INTERVAL ) { // user has not drunk enough so the snooze-remind start every two minutes (until enough water was drunk)
+		Serial.println("Snooze Snooze Snooze Snooze!!!!");
+		remindToDrink();
+		lastSnoozeMillis = millis();
+	}
+	int glassState = getGlassState();
+
+	switch (deviceState){
+		case STANDARD:
+			Serial.println("STANDARD");
+			if( glassState ==  -1 ){ // no glass on plate
+				deviceState = NO_GLASS;
+			}
+			else if ( glassState == 0 ) { // glass empty
+				attachInterrupt(digitalPinToInterrupt(FLOW_SENSOR_PIN), pulse, RISING);
+				if(wasFilledUp){
+					waterAmount_drunk += WATER_AMOUNT;
+				}
+				wasFilledUp = true;
+				digitalWrite(WATER_PUMP_PIN, HIGH); // start water pump
+				deviceState = RECHARGING;
+			}
+			break;
+
+		case RECHARGING:
+			Serial.println("RECHARGING");
+			if ( glassState ==  -1 ){
+				fillingInterrupted = true;
+				digitalWrite(WATER_PUMP_PIN, LOW); // stop water pump
+				deviceState = NO_GLASS;
+			}
+			 while ( bottleEmpty() ) { // wait until the bottle is filled up
+				runningPixel(bottleStrip, bottleStrip.Color(255, 0, 0), 20);
+			}
+			if ( waterFlow >= WATER_AMOUNT ){ // 80 ml were filled in the glass
+				detachInterrupt(digitalPinToInterrupt(FLOW_SENSOR_PIN)) ;
+				digitalWrite(WATER_PUMP_PIN, LOW); // stop water pump
+				waterFlow = 0;
+				deviceState = STANDARD;
+			}
+			break;
+
+		case NO_GLASS:
+			Serial.println("NO_GLASS");
+			if ( glassState !=  -1 ) {
+				if(fillingInterrupted){ // if filling was interrupted, the program jumps back to the previous state
+					fillingInterrupted = false;
+					digitalWrite(WATER_PUMP_PIN, HIGH); // restart water pump
+					deviceState = RECHARGING;
+				}
+				else{
+					deviceState = STANDARD;
+				}
+			}
+			//delay(1000); // delay achieves that no random values are measured when putting down glass
+			break;
+	}
+	//delay(5);
 }
